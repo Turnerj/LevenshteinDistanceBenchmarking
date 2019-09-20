f0f8bc4a87c14fec76e9a74010a54f8d095c7b3f
Turnerj <git@turnerj.com>
2019-09-20 12:38:37 +0930

    Added quad op intrinsic calculator

---
+++ LevenshteinDistanceBenchmarking/Implementations/Alternatives/QuadOpIntrinsicCalculator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+
+namespace LevenshteinDistanceBenchmarking.Implementations.Alternatives
+{
+	class QuadOpIntrinsicCalculator : ILevenshteinDistanceCalculator
+	{
+		public unsafe int CalculateDistance(string source, string target)
+		{
+			var costMatrix = Enumerable
+			  .Range(0, source.Length + 1)
+			  .Select(line => new int[target.Length + 1])
+			  .ToArray();
+
+			for (var i = 1; i <= source.Length; ++i)
+			{
+				costMatrix[i][0] = i;
+			}
+
+			for (var i = 1; i <= target.Length; ++i)
+			{
+				costMatrix[0][i] = i;
+			}
+
+			//var allOnes128Vector = Vector128.Create(1);
+			var allOnes256Vector = Vector256.Create(1);
+
+			for (var i = 1; i <= source.Length; ++i)
+			{
+				//var risingValues = new Span<int>(new int[4] { 0, 1, 2, 3 });
+				//var risingVector = Vector128.Create(0, 1, 2, 3);
+
+				fixed (int* prevRowPtr = costMatrix[i - 1])
+				{
+					for (var j = 1; j <= target.Length; j += 4)
+					{
+						var deletesAndEditsVector = Avx.LoadVector256(prevRowPtr + j - 1);
+						deletesAndEditsVector = Avx2.Add(deletesAndEditsVector, allOnes256Vector);
+
+						var insert1 = costMatrix[i][j - 1] + 1;
+						var delete1 = deletesAndEditsVector.GetElement(1);
+						var edit1 = deletesAndEditsVector.GetElement(0);
+
+						if (source[i - 1] == target[j - 1])
+						{
+							edit1--;
+						}
+
+						var result1 = Math.Min(Math.Min(insert1, delete1), edit1);
+						costMatrix[i][j] = result1;
+
+						if (j == target.Length)
+						{
+							break;
+						}
+
+						var insert2 = result1 + 1;
+						var delete2 = deletesAndEditsVector.GetElement(2);
+						var edit2 = deletesAndEditsVector.GetElement(1);
+
+						if (source[i - 1] == target[j])
+						{
+							edit2--;
+						}
+
+						var result2 = Math.Min(Math.Min(insert2, delete2), edit2);
+						costMatrix[i][j + 1] = result2;
+
+						if (j + 1 == target.Length)
+						{
+							break;
+						}
+
+						var insert3 = result2 + 1;
+						var delete3 = deletesAndEditsVector.GetElement(3);
+						var edit3 = deletesAndEditsVector.GetElement(2);
+
+						if (source[i - 1] == target[j + 1])
+						{
+							edit3--;
+						}
+
+						var result3 = Math.Min(Math.Min(insert3, delete3), edit3);
+						costMatrix[i][j + 2] = result3;
+
+						if (j + 2 == target.Length)
+						{
+							break;
+						}
+
+						var insert4 = result3 + 1;
+						var delete4 = deletesAndEditsVector.GetElement(4);
+						var edit4 = deletesAndEditsVector.GetElement(3);
+
+						if (source[i - 1] == target[j + 2])
+						{
+							edit4--;
+						}
+
+						var result4 = Math.Min(Math.Min(insert4, delete4), edit4);
+						costMatrix[i][j + 3] = result4;
+
+						//risingVector = Sse2.Add(risingVector, allOnesVector);
+					}
+				}
+			}
+
+			return costMatrix[source.Length][target.Length];
+		}
+	}
+}
