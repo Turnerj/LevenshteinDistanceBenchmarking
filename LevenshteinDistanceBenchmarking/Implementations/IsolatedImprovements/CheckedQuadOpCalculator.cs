9f22f602ff62e0d90ffdedddfeb9c548c45d16e9
Turnerj <git@turnerj.com>
2020-02-02 16:18:31 +1030

    Different attempts at Performance Optimizations
    Neither do particularly well. CheckedQuadOp would work a lot better if it used intrinsics.

---
+++ LevenshteinDistanceBenchmarking/Implementations/IsolatedImprovements/CheckedQuadOpCalculator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LevenshteinDistanceBenchmarking.Implementations.IsolatedImprovements
+{
+	class CheckedQuadOpCalculator : ILevenshteinDistanceSpanCalculator
+	{
+		public int CalculateDistance(ReadOnlySpan<char> source, ReadOnlySpan<char> target)
+		{
+			var targetLength = target.Length;
+			var costMatrix = Enumerable
+			  .Range(0, source.Length + 1)
+			  .Select(line => new int[targetLength + 1])
+			  .ToArray();
+
+			for (var i = 1; i <= source.Length; ++i)
+			{
+				costMatrix[i][0] = i;
+			}
+
+			for (var i = 1; i <= target.Length; ++i)
+			{
+				costMatrix[0][i] = i;
+			}
+
+			for (var i = 1; i <= source.Length; ++i)
+			{
+				var jumpSize = 4;
+				var doMultiOp = target.Length > 3;
+				for (var j = 1; j <= target.Length; j += jumpSize)
+				{
+					if (doMultiOp)
+					{
+						var insert1 = costMatrix[i][j - 1] + 1;
+						var delete1 = costMatrix[i - 1][j] + 1;
+						var edit1 = costMatrix[i - 1][j - 1] + (source[i - 1] == target[j - 1] ? 0 : 1);
+
+						var result1 = Math.Min(Math.Min(insert1, delete1), edit1);
+						costMatrix[i][j] = result1;
+
+						var insert2 = result1 + 1;
+						var delete2 = costMatrix[i - 1][j + 1] + 1;
+						var edit2 = costMatrix[i - 1][j] + (source[i - 1] == target[j] ? 0 : 1);
+
+						var result2 = Math.Min(Math.Min(insert2, delete2), edit2);
+						costMatrix[i][j + 1] = result2;
+						
+						var insert3 = result2 + 1;
+						var delete3 = costMatrix[i - 1][j + 2] + 1;
+						var edit3 = costMatrix[i - 1][j + 1] + (source[i - 1] == target[j + 1] ? 0 : 1);
+
+						var result3 = Math.Min(Math.Min(insert3, delete3), edit3);
+						costMatrix[i][j + 2] = result3;
+
+						var insert4 = result3 + 1;
+						var delete4 = costMatrix[i - 1][j + 3] + 1;
+						var edit4 = costMatrix[i - 1][j + 2] + (source[i - 1] == target[j + 2] ? 0 : 1);
+
+						var result4 = Math.Min(Math.Min(insert4, delete4), edit4);
+						costMatrix[i][j + 3] = result4;
+
+						doMultiOp = (target.Length - (j + 4)) > 3;
+					}
+					else
+					{
+						jumpSize = 1;
+						var insert = costMatrix[i][j - 1] + 1;
+						var delete = costMatrix[i - 1][j] + 1;
+						var edit = costMatrix[i - 1][j - 1] + (source[i - 1] == target[j - 1] ? 0 : 1);
+
+						var result = Math.Min(Math.Min(insert, delete), edit);
+						costMatrix[i][j] = result;
+					}
+				}
+			}
+
+			return costMatrix[source.Length][target.Length];
+		}
+	}
+}
---
+++ LevenshteinDistanceBenchmarking/Implementations/IsolatedImprovements/DataStructure/SingleRowStackAllocCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LevenshteinDistanceBenchmarking.Implementations.IsolatedImprovements.DataStructure
+{
+	/// <summary>
+	/// Inspired by "Iosifovich" by Frederik Hertzum
+	/// https://bitbucket.org/clearer/iosifovich/src/master/
+	/// </summary>
+	class SingleRowStackAllocCalculator : ILevenshteinDistanceSpanCalculator
+	{
+		public unsafe int CalculateDistance(ReadOnlySpan<char> source, ReadOnlySpan<char> target)
+		{
+			var previousRow = stackalloc int[target.Length + 1];
+
+			for (var i = 1; i <= target.Length; ++i)
+			{
+				previousRow[i] = i;
+			}
+
+			for (var i = 1; i <= source.Length; ++i)
+			{
+				var previousDiagonal = previousRow[0];
+				var previousColumn = previousRow[0]++;
+
+				for (var j = 1; j <= target.Length; ++j)
+				{
+					var insertOrDelete = Math.Min(previousColumn, previousRow[j]) + 1;
+					var edit = previousDiagonal + (source[i - 1] == target[j - 1] ? 0 : 1);
+
+					previousColumn = Math.Min(insertOrDelete, edit);
+					previousDiagonal = previousRow[j];
+					previousRow[j] = previousColumn;
+				}
+			}
+
+			return previousRow[target.Length];
+		}
+	}
+}
