040c5c42477feaa36354dbefcb569293a9740326
Turnerj <git@turnerj.com>
2020-02-04 10:21:44 +1030

    Character comparison shortcut - creates a fast path for character match

---
+++ LevenshteinDistanceBenchmarking/Implementations/IsolatedImprovements/CharComparisonShortcutCalculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LevenshteinDistanceBenchmarking.Implementations.IsolatedImprovements
+{
+	public class CharComparisonShortcutCalculator : ILevenshteinDistanceSpanCalculator
+	{
+		public int CalculateDistance(ReadOnlySpan<char> source, ReadOnlySpan<char> target)
+		{
+			var targetLength = target.Length;
+			var costMatrix = Enumerable
+			  .Range(0, source.Length + 1)
+			  .Select(line => new int[targetLength + 1])
+			  .ToArray();
+
+			for (var i = 1; i <= source.Length; ++i)
+			{
+				costMatrix[i][0] = i;
+			}
+
+			for (var i = 1; i <= target.Length; ++i)
+			{
+				costMatrix[0][i] = i;
+			}
+
+			for (var i = 1; i <= source.Length; ++i)
+			{
+				for (var j = 1; j <= target.Length; ++j)
+				{
+					if (source[i - 1] == target[j - 1])
+					{
+						costMatrix[i][j] = costMatrix[i - 1][j - 1];
+					}
+					else
+					{
+						var insert = costMatrix[i][j - 1];
+						var delete = costMatrix[i - 1][j];
+						var edit = costMatrix[i - 1][j - 1];
+
+						costMatrix[i][j] = Math.Min(Math.Min(insert, delete), edit) + 1;
+					}
+				}
+			}
+
+			return costMatrix[source.Length][target.Length];
+		}
+	}
+}
